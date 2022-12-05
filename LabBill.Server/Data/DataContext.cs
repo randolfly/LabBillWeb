e689fea87dec5d622e5e72ba1a106ac03caf2bdd
randolf <zrq21@mails.tsinghua.edu.cn>
2022-12-05 23:14:35 +0800

    完成拖拽上传组件

--- LabBill.Server/Data/DataContext.cs
+++ LabBill.Server/Data/DataContext.cs
@@ -6,9 +6,13 @@ namespace LabBill.Server.Data;
 
 public class DataContext : DbContext {
     public DbSet<Bill> Bills { get; set; }
+
     public DbSet<Person> Persons { get; set; }
+
     public DbSet<BillPerson> BillPersons { get; set; }
+
     public DbSet<Asset> Assets { get; set; }
+
     public DbSet<BillType> BillTypes { get; set; }
 
     public static readonly ILoggerFactory ConsoleLoggerFactory =
@@ -19,77 +23,33 @@ public class DataContext : DbContext {
                 .AddConsole();
         });
 
-    public DataContext(DbContextOptions<DataContext> options) : base(options)
-    {
-    }
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
         modelBuilder.Entity<BillPerson>().HasKey(x => new
-            {
-                x.BillId,
-                x.PersonId
-            }
-        );
+        {
+            x.BillId,
+            x.PersonId
+        });
 
 
         var persons = new List<Person>
         {
-            new Person
-            {
-                Id = 1,
-                Name = "randolf"
-            },
-            new Person
-            {
-                Id = 2,
-                Name = "pdd"
-            },
-            new Person
-            {
-                Id = 3,
-                Name = "张荣侨"
-            },
-            new Person
-            {
-                Id = 4,
-                Name = "YXY"
-            }
+            new Person { Id = 1, Name = "randolf" },
+            new Person { Id = 2, Name = "pdd" },
+            new Person { Id = 3, Name = "张荣侨" },
+            new Person { Id = 4, Name = "YXY" }
         };
         modelBuilder.Entity<Person>().HasData(persons);
 
         var billTypes = new List<BillType>
         {
-            new BillType
-            {
-                Id = 1,
-                Name = "食品"
-            },
-            new BillType
-            {
-                Id = 2,
-                Name = "设备"
-            },
-            new BillType
-            {
-                Id = 3,
-                Name = "娱乐"
-            },
-            new BillType
-            {
-                Id = 4,
-                Name = "知识"
-            },
-            new BillType
-            {
-                Id = 5,
-                Name = "工资"
-            },
-            new BillType
-            {
-                Id = 6,
-                Name = "其余"
-            }
+            new BillType { Id = 1, Name = "食品" },
+            new BillType { Id = 2, Name = "设备" },
+            new BillType { Id = 3, Name = "娱乐" },
+            new BillType { Id = 4, Name = "知识" },
+            new BillType { Id = 5, Name = "工资" },
+            new BillType { Id = 6, Name = "其余" }
         };
 
         modelBuilder.Entity<BillType>().HasData(billTypes);
--- LabBill.Shared/Helper/BillPersonModifyMap.cs
+++ LabBill.Shared/Helper/BillPersonModifyMap.cs
@@ -2,6 +2,7 @@
 
 namespace LabBill.Shared.Helper; 
 
+// 删除1个BillPerson联系，添加多个BillPerson联系
 public class BillPersonModifyMap {
     public BillPerson billPerson { get; set; }
     public List<BillPerson> targetBillPersons { get; set; }
